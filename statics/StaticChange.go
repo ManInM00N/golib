0b5c36b15f2472b66cb274e81bc200f6938ed530
ManInM00N <571404393@qq.com>
2023-12-11 23:08:16 +0800

    static

---
+++ statics/StaticChange.go
@@ -0,0 +1,16 @@
+package statics
+
+func StringToInt64(s string) int64 {
+	var num int64 = 0
+	for i := 0; i < len(s); i++ {
+		num = num*10 + int64(s[i]-'0')
+	}
+	return num
+}
+func Int64ToString(num int64) string {
+	var s string = ""
+	for ; num > 0; num /= 10 {
+		s = s + string(num%10+'0')
+	}
+	return s
+}
