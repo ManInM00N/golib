50b9cc45c85c0f5515a823251f1d0ebb8a8d0a30
ManInM00N <571404393@qq.com>
2023-12-26 20:44:54 +0800

    goroutine

--- goruntine/gopool.go
+++ goruntine/gopool.go
@@ -1,15 +1,20 @@
 package goruntine
 
+import "sync"
+
 type GoPool struct {
-	n int
-	c chan struct{}
+	n  int
+	c  chan struct{}
+	wg sync.WaitGroup
 }
 
-func NewGoPool(n int) *GoPool {
+func NewGoPool(n int, worker int) *GoPool {
+	wg.Add(worker)
 	return &GoPool{
 		n: n,
 		c: make(chan struct{}, n),
 	}
+
 }
 func (g *GoPool) add() {
 	g.c <- struct{}{}
@@ -19,6 +24,10 @@ func (g *GoPool) pop() {
 }
 func (g *GoPool) Run(f func()) {
 	g.add()
+	defer g.wg.Done()
 	f()
 	g.pop()
+}
+func (g *GoPool) Wait() {
+	g.wg.Wait()
 }
