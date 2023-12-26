6c90184b06dc95806b5f6481f210a01c81c28614
ManInM00N <571404393@qq.com>
2023-12-26 22:27:38 +0800

    none

--- goruntine/gopool.go
+++ goruntine/gopool.go
@@ -4,32 +4,38 @@ import "sync"
 
 type GoPool struct {
 	n  int
-	c  chan struct{}
+	c  chan func
 	wg sync.WaitGroup
 }
-
+type Task func()
 func NewGoPool(n int, worker int) *GoPool {
 	p := &GoPool{
 		n: n,
-		c: make(chan struct{}, n),
+		c: make(chan Task, n),
 	}
 	p.wg.Add(worker)
 	return p
 }
-func (g *GoPool) add() {
-	g.c <- struct{}{}
+func (g *GoPool) Add(task Task ) {
+	g.c <- task
 }
-func (g *GoPool) pop() {
+func (g *GoPool) Pop() {
 	<-g.c
 }
-func (g *GoPool) Run(f func()) {
-	g.add()
-	f()
-	g.pop()
+func (g *GoPool) Run() {
+	go func(){
+		defer g.Done()
+		for task:=range g.c{
+			task()
+		}
+	}()
 }
 func (g *GoPool) Done() {
 	g.wg.Done()
 }
 func (g *GoPool) Wait() {
 	g.wg.Wait()
+}
+func (g *GoPool)Close(){
+	close(c)
 }
