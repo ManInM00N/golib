cbbd0160c12ae27cc28ce9c59e50d14887e167f3
ManInM00N <571404393@qq.com>
2025-08-25 03:08:30 +0800

    feat: worker & multi_task

--- goruntine/TaskPool.go
+++ goruntine/TaskPool.go
@@ -2,14 +2,21 @@ package goruntine
 
 import (
 	"context"
+	"sync"
 
 	. "github.com/ManInM00N/go-tool/heap"
 )
 
 type TaskPool struct {
-	n     int
-	c     chan task
-	queue *PriorityQueue[task]
+	n             int
+	c             chan task
+	wg            sync.WaitGroup
+	queue         *PriorityQueue[task]
+	cond          *sync.Cond
+	maxConcurrent int
+	mu            sync.Mutex
+	sem           chan struct{}
+	running       bool
 }
 type task struct {
 	val   int
@@ -32,10 +39,11 @@ func NewTask(val int) (task, context.CancelFunc) {
 	return task, cancel
 }
 
-func NewTaskPool(worker int) *TaskPool {
+func NewTaskPool(worker int, maxConcurrent int) *TaskPool {
 	temp := &TaskPool{
-		n: worker,
-		c: make(chan task),
+		n:   worker,
+		c:   make(chan task),
+		sem: make(chan struct{}, maxConcurrent),
 		queue: NewPriorityQueue[task](func(i, j task) bool {
 			return i.val < j.val
 		}),
@@ -44,16 +52,68 @@ func NewTaskPool(worker int) *TaskPool {
 }
 
 func (P *TaskPool) Run() {
-	defer close(P.c)
-	for {
-		select {
-		case v := <-P.c:
-			{
-				P.queue.Push(v)
-			}
-		default:
-			{
-			}
-		}
+	P.running = true
+	for i := 0; i < P.n; i++ {
+		go P.worker()
 	}
+}
+func (p *TaskPool) worker() {
+	for {
+		p.mu.Lock()
+		for p.queue.Len() == 0 && p.running {
+			p.cond.Wait()
+		}
+		if !p.running && p.queue.Len() == 0 {
+			p.mu.Unlock()
+			return
+		}
+		t := p.queue.Pop().(task)
+		p.mu.Unlock()
+
+		p.sem <- struct{}{}
+		p.wg.Add(1)
+		go func(t task) {
+			defer func() {
+				<-p.sem
+				p.wg.Done()
+			}()
+			t.Inner()
+		}(t)
+	}
+}
+
+func (g *TaskPool) AddWorker(num int) {
+	g.wg.Add(num)
+}
+func (g *TaskPool) NewTask(fn func(), info interface{}, val int) (task, context.CancelFunc) {
+	t, cancel := NewTask(val)
+	t.Inner = fn
+	t.Info = info
+	return t, cancel
+}
+func (p *TaskPool) Add(t task) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	p.queue.Push(t)
+	p.cond.Signal()
+}
+
+func (p *TaskPool) Stop() {
+	p.mu.Lock()
+	p.running = false
+	p.mu.Unlock()
+	p.cond.Broadcast()
+	p.wg.Wait()
+}
+func (g *TaskPool) Pop() {
+	<-g.c
+}
+func (g *TaskPool) Done() {
+	g.wg.Done()
+}
+func (g *TaskPool) Wait() {
+	g.wg.Wait()
+}
+func (g *TaskPool) Close() {
+	close(g.c)
 }
